ccfc21b308afee78ef0c7f50ff5bc7737de5a4fa
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-10-14 22:31:14 -0400

    changes index.js to class component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,18 @@
-import React from "react"
+import React, { Component } from "react";
 import Layout from '../components/layout';
 import Folio from '../components/folio';
 
-export default function Home() {
-  return (
-    <div>
-      <Layout>
-        <Folio />
-        <Folio />
-      </Layout>
-    </div>
-  )
+class Home extends Component () {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Folio />
+          <Folio />
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default Home;
