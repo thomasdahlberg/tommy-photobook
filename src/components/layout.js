988d05e3afe5cd074dc064a6c9421c1cdd032044
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2021-02-23 21:24:00 -0500

    Edits Layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
-import styles from '../styles/layout.module.scss';
 
 const Layout = (props) => {
     return (
